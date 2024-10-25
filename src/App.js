26d8076abb8f264fe24a8fa16bdc56d1ac79e872
bishwanath4me <bishwanath3714312@gmail.com>
2024-10-25 14:16:40 +0530

    use callback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./App.css";
 import Navbar from "../src/components/Navbar";
 import WeatherCard from "../src/components/WeatherCard";
@@ -6,15 +6,12 @@ import axios from "axios";
 import TodayData from "./components/TodayData";
 import FiveDayForecast from "./components/Fiveday";
 
-
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState("Delhi");
   const [airQualityData, setAirQualityData] = useState(null);
   const [fiveDayForecast, setFiveDayForecast] = useState(null);
 
-
-  
   const AirQualityData = (lat, lon) => {
     const API_KEY = "1f5a2fa85d07783032272b2c7e91acef"; // Replace with your OpenWeatherMap API key
     axios
@@ -29,15 +26,19 @@ function App() {
       );
   };
 
-  const WeatherDatas = async () => {
+  const WeatherDatas = useCallback(async () => {
     const API_KEY = "1f5a2fa85d07783032272b2c7e91acef";
-   await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+    await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    )
       .then((response) => response.json())
       .then((data) => {
         setWeatherData(data);
-        console.log(JSON.stringify(data));
         AirQualityData(data.coord.lat, data.coord.lon);
-        axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`)
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`
+          )
           .then((response) => {
             setFiveDayForecast(response.data);
           })
@@ -48,36 +49,32 @@ function App() {
       .catch((error) =>
         console.error("Error fetching the weather data:", error)
       );
-  };
+  }, [city]);
 
   useEffect(() => {
-    WeatherDatas(city);
-  }, [city]); 
+    WeatherDatas();
+  }, [WeatherDatas]);
 
   const handleSearch = (searchedCity) => {
     setCity(searchedCity);
   };
+
   return (
     <div>
       <Navbar onSearch={handleSearch} />
       {weatherData && airQualityData && (
-        <div className="weathercontainer" style={{backgroundColor:"#90a8ce"}}>
+        <div className="weathercontainer" style={{ backgroundColor: "#90a8ce" }}>
           <div className="cardContainer">
             <WeatherCard weatherData={weatherData} />
-             <p
+            <p
               style={{ fontWeight: "700", fontSize: "20px", marginTop: "20px" }}
             >
               5 Days Forecast
-            </p> 
-             {fiveDayForecast && (
-              <FiveDayForecast forecastData={fiveDayForecast} />
-            )}
+            </p>
+            {fiveDayForecast && <FiveDayForecast forecastData={fiveDayForecast} />}
           </div>
           <div className="todayRoot">
-            <TodayData
-              weatherData={weatherData}
-              airQualityData={airQualityData}
-            />
+            <TodayData weatherData={weatherData} airQualityData={airQualityData} />
           </div>
         </div>
       )}
